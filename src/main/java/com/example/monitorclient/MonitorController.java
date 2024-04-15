1f565ad60a07c5cd25377c5ebd046980254f41d8
Black-Emblem <kastanias_g@outlook.com>
2024-04-15 16:24:44 +0300

    v0.0.1.1

--- src/main/java/com/example/monitorclient/MonitorController.java
+++ src/main/java/com/example/monitorclient/MonitorController.java
@@ -1,12 +1,21 @@
 package com.example.monitorclient;
 
-import javafx.concurrent.Worker;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Objects;
-
 
 public class MonitorController {
 
@@ -15,10 +24,36 @@ public class MonitorController {
 
     @FXML
     public void initialize() {
+        myWebView.sceneProperty().addListener(new ChangeListener<javafx.scene.Scene>() {
+            @Override
+            public void changed(ObservableValue<? extends javafx.scene.Scene> observable, javafx.scene.Scene oldValue, javafx.scene.Scene newValue) {
+                if (newValue != null) {
+                    newValue.getAccelerators().put(
+                            new KeyCodeCombination(KeyCode.M, KeyCombination.CONTROL_DOWN),
+                            () -> {
+                                try {
+                                    FXMLLoader loader = new FXMLLoader(getClass().getResource("popup.fxml"));
+                                    Parent root = loader.load();
+                                    Stage stage = new Stage();
+                                    stage.setScene(new Scene(root));
+                                    stage.showAndWait();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                    );
+                }
+            }
+        });
+
         WebEngine engine = myWebView.getEngine();
-        engine.setJavaScriptEnabled(true);
         engine.setUserStyleSheetLocation(Objects.requireNonNull(getClass().getResource("/hideScrollBar.css")).toExternalForm());
         engine.load("https://nowrealestate.gr/property/6066");
-
+    }
+    @FXML
+    public void Forcedurl(String URL) {
+        WebEngine engine = myWebView.getEngine();
+        engine.setUserStyleSheetLocation(Objects.requireNonNull(getClass().getResource("/hideScrollBar.css")).toExternalForm());
+        engine.load(URL);
     }
 }
--- src/main/java/com/example/monitorclient/database/dbconn.java
+++ src/main/java/com/example/monitorclient/database/dbconn.java
@@ -3,16 +3,35 @@ package com.example.monitorclient.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class dbconn {
     static Connection conn = null;
 
     public static void initializeConn() {
-        if (conn == null){
-            final String DB_URL = "jdbc:mariadb://192.168.1.8:3306/MMdb";
+        if (conn == null){String DB_URL = "";
+            String USER = "";
+            String PASS = "";
 
-            final String USER = "admin";
-            final String PASS = "admin";
+            // Read data from CSV file
+            try {
+                Scanner scanner = new Scanner(new File("data.csv"));
+                while (scanner.hasNext()) {
+                    String[] data = scanner.next().split(",");
+                    String ip = data[2];
+                    String port = data[3];
+                    String dbName = data[1];
+                    USER = data[4];
+                    PASS = data[5];
+
+                    DB_URL = "jdbc:mariadb://" + ip + ":" + port + "/" + dbName;
+                }
+                scanner.close();
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException("File not found", e);
+            }
 
             try {
                 //Creating a connection to the database
@@ -26,26 +45,26 @@ public class dbconn {
 
     public static Connection db_conn() throws SQLException {
         if (conn == null){
-            final String DB_URL = "jdbc:mariadb://192.168.1.8:3306/MMdb";
-
-            final String USER = "admin";
-            final String PASS = "admin";
-
-            try {
-                //Creating a connection to the database
-                conn = DriverManager.getConnection(DB_URL, USER, PASS);
-                System.out.println("Connection successful");
-            } catch (SQLException e) {
-                throw new RuntimeException("Connection with database failed", e);
-            }
-            return  conn;
-        } else {
+            initializeConn();
             return conn;
+        } else {
+            try {
+                if(conn != null && !conn.isClosed()) {
+                    initializeConn();
+                    return conn;
+                } else {
+                    return conn;
+                }
+            } catch(SQLException e){
+                return conn;
+            }
         }
     }
 
     // Method to test connection with database
     public static String testConnection() {
+        conn=null;
+        initializeConn();
         try {
             if(conn != null && !conn.isClosed()) {
                 return "Connection with database is successful.";
